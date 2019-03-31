08d5d72ed996541e03008b9f92c59891257269f8
YuFei <yufei5367@163.com>
2019-03-31 19:08:23 +0800

    初始化

---
+++ hello-spring/src/main/java/geektime/spring/hello/hellospring/HelloSpringApplication_1.java
@@ -0,0 +1,11 @@
+package geektime.spring.hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApplication_1 {
+    public static void main(String[] args){
+        SpringApplication.run(HelloSpringApplication_1.class,args);
+    }
+}
---
+++ hello-spring/src/test/java/geektime/spring/hello/hellospring/HelloSpringApplicationTests_1.java
@@ -0,0 +1,10 @@
+package geektime.spring.hello.hellospring;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloSpringApplicationTests_1 {
+}
